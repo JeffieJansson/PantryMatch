7fc6dc1fb142db3844ae5e64130ee9f7bbb48434
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-02-10 14:34:13 +0100

    Add User model schema

---
+++ backend/model/User.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import crypto from "crypto";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  accessToken: {
+    type: String,
+    required: true,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
