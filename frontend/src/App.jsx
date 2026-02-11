8300e5c33a54a1b3a4684147317581acd9b7f797
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-02-11 17:53:06 +0100

    add routing and user state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/home";
+import { useUserStore } from "./stores/userStore";
+import About from "./pages/about";
+
+
 export const App = () => {
 
+  const { user, setUser, logout } = useUserStore();
+  const [isSigningUp, setIsSigningUp] = useState(false);
+
+
   return (
-    <>
-      <h1>Welcome to Final Project!</h1>
-    </>
+    <BrowserRouter>
+
+      <Navigation />
+
+      <Routes>
+        
+        <Route
+          path="/"
+          element={
+            <Home
+              user={user}
+              isSigningUp={isSigningUp}
+              setIsSigningUp={setIsSigningUp}
+              handleLogin={setUser}
+              handleLogout={logout}
+            />
+          }
+        />
+
+
+        {/* Protected route */}
+        <Route
+          path="/saved"
+          element={
+            user ? <h2>Saved Recipes</h2> : <Navigate to="/" />
+          }
+
+        />
+
+
+        <Route
+          path="/about"
+          element={
+            <About />
+          }
+        />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 };
