6ff92b3753b25a53f2df789a208b3c60b4f55ba2
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-02-18 16:32:45 +0100

    add setError for minimun ingredient input and add remove ingredient

--- frontend/src/hooks/useRecipeActions.js
+++ frontend/src/hooks/useRecipeActions.js
@@ -3,16 +3,30 @@ import { fetchRecipeByIngredients } from "../api/api";
 
 export const useRecipeActions = () => {
   const {
-    ingredients, recipes, mode, loading, error,
-    setRecipes, setLoading, setError, setMode, addIngredient,
+    ingredients,
+    recipes,
+    mode, 
+    loading,
+    error,
+    setRecipes,
+    setLoading,
+    setError,
+    setMode,
+    addIngredient,
+    removeIngredient,
   } = useRecipeStore();
 
-
   const searchRecipes = async () => {
+    if (ingredients.length < 1) {
+      setError("Add at least 1 ingredient");
+      return;
+    }
+
     setLoading(true);
     setError(null);
+
     try {
-      const data = await fetchRecipeByIngredients(ingredients, mode);
+      const data = await fetchRecipeByIngredients(ingredients, mode); // send mode to API
       setRecipes(data);
     } catch (err) {
       setError(err.message);
@@ -23,7 +37,14 @@ export const useRecipeActions = () => {
   };
 
   return {
-    ingredients, recipes, mode, loading, error,
-    setMode, addIngredient, searchRecipes
+    ingredients,
+    recipes,
+    mode,
+    loading,
+    error,
+    setMode,
+    addIngredient,
+    removeIngredient,
+    searchRecipes,
   };
 };
